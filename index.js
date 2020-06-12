b0c81492825084c5b84221cfb169370cac95955e
Revan Sarbast <revansarbast8@gmail.com>
2020-06-12 12:57:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 // Write your code here
 class Breakfast{
-  
+  let _food;
+  let _drink;
+  constructor(food = 'eggs', drink ='tea'){
+    this._food = food;
+    this._drink = drink
+  }
 }
 class Lunch{
-  
+  constructor(){
+    
+  }
 }
 class Dinner{
-  
+  constructor(){
+    
+  }
 }
